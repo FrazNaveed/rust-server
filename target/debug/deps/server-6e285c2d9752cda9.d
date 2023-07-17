cb6b8c5febcd118a7ba1cbef611fb549d379e986
fraz <fraz@gmail.com>
2023-07-17 11:35:34 +0500

    defined project structure & added deps

---
+++ target/debug/deps/server-6e285c2d9752cda9.d
@@ -0,0 +1,5 @@
+/Users/fraznaveed/Desktop/server/target/debug/deps/server-6e285c2d9752cda9.rmeta: src/main.rs
+
+/Users/fraznaveed/Desktop/server/target/debug/deps/server-6e285c2d9752cda9.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/server-d4c732b7924d7a36.d
@@ -0,0 +1,5 @@
+/Users/fraznaveed/Desktop/server/target/debug/deps/server-d4c732b7924d7a36.rmeta: src/main.rs
+
+/Users/fraznaveed/Desktop/server/target/debug/deps/server-d4c732b7924d7a36.d: src/main.rs
+
+src/main.rs:
